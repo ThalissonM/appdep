6fa711d281602082f9795dec31321481bc873f39
ThalissonM <munizepinheiro@hotmail.com>
2017-07-09 20:13:22 -0300

    Adicionar arquivos de projeto.

---
+++ appdep/scripts/application.ts
@@ -0,0 +1,29 @@
+﻿// Para uma introdução ao modelo em branco, consulte a seguinte documentação:
+// http://go.microsoft.com/fwlink/?LinkID=397705
+// Para depurar códigos no carregamento de página em dispositivos/emuladores Android ou que simulam o Cordova: inicie o aplicativo, defina os pontos de interrupção 
+// e execute "window.location.reload()" no Console do JavaScript.
+"use strict";
+
+export function initialize(): void {
+    document.addEventListener('deviceready', onDeviceReady, false);
+}
+
+function onDeviceReady(): void {
+    document.addEventListener('pause', onPause, false);
+    document.addEventListener('resume', onResume, false);
+
+    // TODO: o Cordova foi carregado. Execute qualquer inicialização que exija o Cordova aqui.
+    var parentElement = document.getElementById('deviceready');
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+}
+
+function onPause(): void {
+    // TODO: este aplicativo foi suspenso. Salve o estado do aplicativo aqui.
+}
+
+function onResume(): void {
+    // TODO: este aplicativo foi reativado. Restaure o estado do aplicativo aqui.
+}
---
+++ appdep/scripts/startup.ts
@@ -0,0 +1,9 @@
+﻿import * as Application from './application';
+
+declare var require: (modules: string[], ready: Function, errback: Function) => void;
+
+// Tente carregar o código específico da plataforma na pasta /merges.
+// Mais informações em http://taco.visualstudio.com/pt-br/docs/configure-app/#Content.
+require(["./platformOverrides"],
+    () => Application.initialize(),
+    () => Application.initialize());
